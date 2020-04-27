b202a9441386a10f45f48dcea053b27a4d1c1ec8
WideanNagari <Wideannagari@gmail.com>
2020-04-27 14:35:19 +0700

    test commit

---
+++ src/strike/pkg1945/Strike1945.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package strike.pkg1945;
+
+/**
+ *
+ * @author Widean Nagari
+ */
+public class Strike1945 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
