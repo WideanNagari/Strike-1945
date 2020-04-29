cdd40dfaa436f58e54a9b172203152efdd97ffff
WideanNagari <Wideannagari@gmail.com>
2020-04-29 19:55:28 +0700

    New Game, Continue, High Score

--- src/strike/pkg1945/Strike1945.java
+++ src/strike/pkg1945/Strike1945.java
@@ -16,7 +16,7 @@ public class Strike1945 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        About main = new About();
+        NewGame main = new NewGame();
         main.pack();
         main.setLocationRelativeTo(null);
         main.setVisible(true);
