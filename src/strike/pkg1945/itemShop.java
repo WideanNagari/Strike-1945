638b1a0b92041b3d3e3c55c12a2aa62378f8e5db
JeffreyAdrian27 <jeffreyadrian27@gmail.com>
2020-05-03 13:03:47 +0700

    Revert "Revert "itemshop""
    This reverts commit d5576d160b4c7a40f69a4e19987d78c6baf02b4e.

---
+++ src/strike/pkg1945/itemShop.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package strike.pkg1945;
+
+/**
+ *
+ * @author Jeffrey A
+ */
+public class itemShop {
+    private int hargaEaglePotion;
+    private int hargaShieldofCrystalBarrier;
+    private int hargaMassiveSalve;
+    private int hargaAngelBox;
+    private int hargaSackofGold;
+    private int jumShieldofCrystal=1;
+    private int jumMassiveSalve=1;
+    private int jumSackofGold=1;
+    private int jumAngleBox=1;
+    private int jumEaglePotion=1;
+
+    public itemShop() {
+        this.hargaEaglePotion = 1200;
+        this.hargaShieldofCrystalBarrier = 700;
+        this.hargaMassiveSalve = 500;
+        this.hargaAngelBox = 2500;
+        this.hargaSackofGold=500;
+        
+    }
+
+    public int getHargaEaglePotion() {
+        return hargaEaglePotion;
+    }
+
+    public int getHargaShieldofCrystalBarrier() {
+        return hargaShieldofCrystalBarrier;
+    }
+
+    public int getHargaMassiveSalve() {
+        return hargaMassiveSalve;
+    }
+
+    public int getHargaAngelBox() {
+        return hargaAngelBox;
+    }
+
+    public int getHargaSackofGold() {
+        return hargaSackofGold;
+    }
+
+    public int getJumShieldofCrystal() {
+        return jumShieldofCrystal;
+    }
+
+    public int getJumMassiveSalve() {
+        return jumMassiveSalve;
+    }
+
+    public int getJumSackofGold() {
+        return jumSackofGold;
+    }
+
+    public int getJumAngleBox() {
+        return jumAngleBox;
+    }
+
+    public int getJumEaglePotion() {
+        return jumEaglePotion;
+    }
+    
+    
+   
+    
+    
+    
+    
+    
+    
+}
